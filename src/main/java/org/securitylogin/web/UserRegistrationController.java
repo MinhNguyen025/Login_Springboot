28899a32ebc7a90bd908f22de8710c2dbe8ffd9b
MinhWuyn <nguyenduyminh0205@gmail.com>
2024-06-13 22:31:11 +0700

    Connect to mySQL and add first register

--- src/main/java/org/securitylogin/web/UserRegistrationController.java
+++ src/main/java/org/securitylogin/web/UserRegistrationController.java
@@ -4,7 +4,9 @@ import org.securitylogin.service.UserService;
 import org.securitylogin.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +20,17 @@ public class UserRegistrationController {
         this.userService = userService;
     }
 
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
         return "redirect:/registration?success";
